5a7732435a83945f46ffdc245b314adbd942a500
leilani-heather <leilaniheather@gmail.com>
2023-02-08 13:41:09 +1300

    remove gitignore files

--- dist/magic-eight.d.ts
+++
@@ -1,9 +0,0 @@
-declare let userName: string;
-declare let userQuestion: string;
-declare const submitUserName: () => void;
-declare const hide: (element: string) => void;
-declare const show: (element: string) => void;
-declare const greetUser: (userName: string) => void;
-declare const askQuestion: () => void;
-declare const doMagic: () => void;
-declare const askAgain: () => void;
